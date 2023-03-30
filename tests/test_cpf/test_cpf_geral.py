53dfe7a7d9d8d28d9b14bf90cff70fd44597193b
Jorge Silva <jorgesilva.ti@hotmail.com>
2023-03-30 10:24:55 -0300

    test: add parametrize cpf test

--- tests/test_cpf/test_cpf_geral.py
+++ tests/test_cpf/test_cpf_geral.py
@@ -2,79 +2,115 @@ import re
 
 import pytest
 from faker import Faker
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 
-from pydantic_br import CPF, CPFDigits, CPFMask
+from pydantic_br import (
+    CPF,
+    CPFDigitError,
+    CPFDigits,
+    CPFInvalidError,
+    CPFMask,
+    CPFMaskError,
+    CPFTypeError,
+)
 
-TOTAL_CPF = 10
+TOTAL_CPF = 4
 fake = Faker("pt-BR")
 
 
-def cpf_digitos():
+def cpf_digits():
     cpfs = [re.sub("[^0-9]", "", fake.cpf()) for _ in range(TOTAL_CPF)]
     return cpfs
 
 
-def cpf_mascaras():
+def cpf_mask():
     cpfs = [fake.cpf() for _ in range(TOTAL_CPF)]
     return cpfs
 
 
-def cpf_misturados():
+def cpf_mixed():
     cpfs = [fake.cpf() for _ in range(int(TOTAL_CPF / 2))]
     cpfs += [re.sub("[^0-9]", "", fake.cpf()) for _ in range(int(TOTAL_CPF / 2))]
     return cpfs
 
 
 @pytest.fixture
-def pessoa():
-    class Pessoa(BaseModel):
+def person():
+    class Person(BaseModel):
         cpf: CPF
 
-    yield Pessoa
+    yield Person
 
 
 @pytest.fixture
-def pessoa_com_mascara():
-    class Pessoa(BaseModel):
+def person_masks():
+    class Person(BaseModel):
         cpf: CPFMask
 
-    yield Pessoa
+    yield Person
 
 
 @pytest.fixture
-def pessoa_somente_digitos():
-    class Pessoa(BaseModel):
+def person_digits():
+    class Person(BaseModel):
         cpf: CPFDigits
 
-    yield Pessoa
+    yield Person
 
 
-@pytest.mark.parametrize("cpf", cpf_misturados())
-def test_cpf_deve_ser_string(pessoa, cpf):
-    p1 = pessoa(cpf=cpf)
+@pytest.mark.parametrize("cpf", cpf_mixed())
+def test_must_be_string(person, cpf):
+    p1 = person(cpf=cpf)
     assert isinstance(p1.cpf, str)
 
 
-@pytest.mark.parametrize("cpf", cpf_mascaras())
-def test_cpf_mascara_deve_ser_string(pessoa_com_mascara, cpf):
-    p1 = pessoa_com_mascara(cpf=cpf)
+@pytest.mark.parametrize("cpf", cpf_mask())
+def test_mascara_must_be_string(person_masks, cpf):
+    p1 = person_masks(cpf=cpf)
     assert isinstance(p1.cpf, str)
 
 
-@pytest.mark.parametrize("cpf", cpf_digitos())
-def test_cpf_digitos_deve_ser_string(pessoa_somente_digitos, cpf):
-    p1 = pessoa_somente_digitos(cpf=cpf)
+@pytest.mark.parametrize("cpf", cpf_digits())
+def test_digits_must_be_string(person_digits, cpf):
+    p1 = person_digits(cpf=cpf)
     assert isinstance(p1.cpf, str)
 
 
-@pytest.mark.parametrize("cpf", cpf_mascaras())
-def test_cpf_must_accept_with_mask(pessoa_com_mascara, cpf):
-    p1 = pessoa_com_mascara(cpf=cpf)
+@pytest.mark.parametrize("cpf", cpf_mask())
+def test_must_accept_with_mask(person_masks, cpf):
+    p1 = person_masks(cpf=cpf)
     assert p1.cpf == cpf
 
 
-@pytest.mark.parametrize("cpf", cpf_digitos())
-def test_cpf_must_accept_only_numbers(pessoa_somente_digitos, cpf):
-    p1 = pessoa_somente_digitos(cpf=cpf)
+@pytest.mark.parametrize("cpf", cpf_digits())
+def test_must_accept_only_numbers(person_digits, cpf):
+    p1 = person_digits(cpf=cpf)
     assert p1.cpf == cpf
+
+
+@pytest.mark.parametrize("cpf", cpf_digits())
+def test_must_fail_when_use_mask_in_digits_class(person_masks, cpf):
+    with pytest.raises(ValidationError) as e:
+        person_masks(cpf=cpf)
+    assert CPFMaskError.msg_template in str(e.value)
+
+
+@pytest.mark.parametrize("cpf", cpf_mask())
+def test_must_fail_when_use_digits_in_mask_class(person_digits, cpf):
+    with pytest.raises(ValidationError) as e:
+        person_digits(cpf=cpf)
+    assert CPFDigitError.msg_template in str(e.value)
+
+
+@pytest.mark.parametrize("cpf", cpf_digits())
+def test_must_fail_when_use_another_type(person_digits, cpf):
+    with pytest.raises(ValidationError) as e:
+        person_digits(cpf=int(cpf))
+    assert CPFTypeError.msg_template in str(e.value)
+
+
+@pytest.mark.parametrize("cpf", cpf_digits())
+def test_must_fail_when_use_invalid_cpfs(person, cpf):
+    with pytest.raises(ValidationError) as e:
+        person(cpf=cpf[-1])
+    assert CPFInvalidError.msg_template in str(e.value)
